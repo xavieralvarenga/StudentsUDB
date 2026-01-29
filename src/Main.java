9c2e450ca8728f5ce5cc08d7789a7cb7c3ff2f3b
Andy Alvarado <AD262401@alumno.udb.edu.sv>
2026-01-29 16:48:51 -0600

    Apartado de busqueda de alumno por carnet

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,24 @@ public class Main {
         // Uso de Scanner para recibir entradas del usuario
         Scanner input = new Scanner(System.in);
 
+        //Variables
+        String nombre;
+        String carnet;
+
         //Variable a utilizar en el menu en validación de switch
         int opcion = 0;
+
+        //Ejemplo de busqueda de alumnos
+        System.out.println("Apartado para buscar alumno por carnet: ");
+        System.out.println("Digite el carnet del alumno que desea buscar: ");
+         carnet = input.nextLine();
+         if(alumnos.containsKey(carnet)){
+            String nombreAlumno = alumnos.get(carnet);
+            System.out.println("Carnet : " + carnet);
+            System.out.println("Nombre : " + nombreAlumno);
+
+         }else{
+             System.out.println("Alumno no encontrado");
+         }
     }
 }
