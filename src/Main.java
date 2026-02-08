5c7eccbb8959494a9fd1b464c8d0b9d346abe87e
Andy Alvarado <AD262401@alumno.udb.edu.sv>
2026-02-07 20:40:44 -0600

    Menu y actualización del alumno

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -9,22 +10,68 @@ public class Main {
          * Estructura recomendada: HashMap<Llave: Carnet, Valor: Nombre>
          * Esta estructura (Map) actuará como la parte de persistencia (BD)
          * */
-        Map<Integer, String> alumnos = new HashMap<>();
-
-        // Pruebas para métodos CRUD previo a crear el menú - [BORRAR]
-        alumnos.put(101, "Usuario Alfa");
-        alumnos.put(102, "Usuario Beta");
-
+        Map<String, String> alumnos = new HashMap<>();
         // Uso de Scanner para recibir entradas del usuario
         Scanner input = new Scanner(System.in);
+        int opcion;
 
-        // Recorrido de Map con Lambda antes de eliminar(pruebas) - [BORRAR]
-        alumnos.forEach((c, n) -> {
-            System.out.println("Carnet: " + c + ", Nombre: " + n);
-        });
+        //Apartado para el menu
+        do{
+            //Aqui tiene que ir el menu
+            System.out.println("\n====================================");
+            System.out.println("      MENÚ DE GESTIÓN (CRUD)        ");
+            System.out.println("====================================");
+            System.out.println("1. Agregar alumno");
+            System.out.println("2. Buscar alumno");
+            System.out.println("3. Actualizar alumno");
+            System.out.println("4. Eliminar alumno");
+            System.out.println("5. Mostrar todos");
+            System.out.println("6. Salir");
+            System.out.println("====================================");
 
-        eliminarAlumno(input, alumnos);
-        //mostrarAlumnos(alumnos);
+            try {
+                System.out.print("Ingrese el opcion: ");
+                opcion = input.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        //Crear el apartado para agregar alumno
+                        System.out.print("Ingrese el alumno: ");
+                        System.out.print("Ingrese el nombre: ");
+                        String nombre = input.next();
+                        System.out.print("Ingrese el carnet: ");
+                        String carnet = input.next();
+
+                        alumnos.put(carnet, nombre);
+                        System.out.println("El alumno se ha agregado correctamente");
+                        break;
+                    case 2:
+                        //apartado para buscar alumnos
+                        buscarAlumno(input, alumnos);
+                        break;
+                    case 3:
+                        //apartado para actualizar alumno
+                        actualizarAlumno(input, alumnos);
+                        break;
+                    case 4:
+                        //apartado para eliminar alumno
+                        eliminarAlumno(input,alumnos);
+                        break;
+                    case 5:
+                        //mostrar todos los alumnos
+                        mostrarAlumnos(alumnos);
+                        break;
+                }
+
+            }catch (Exception e) {
+                System.out.println("Error: Por favor, ingresa solo números.");
+                //Limpia el escaner
+                input.next();
+                //Reinicio del proceso
+                opcion = 0;
+            }
+        }while(opcion != 6);
+
 
     }
 
@@ -36,13 +83,13 @@ public class Main {
      * El método recibe el Map de alumnos como parámetro
      * Para definir en que colección buscar y obtener el valor del nombre del alumno
      * */
-    private static void buscarAlumno(Scanner input, Map<Integer, String> alumnos) {
-        int carnet;
+    private static void buscarAlumno(Scanner input, Map<String, String> alumnos) {
+
 
         //Ejemplo de busqueda de alumnos
         System.out.println("Apartado para buscar alumno por carnet: ");
         System.out.println("Digite el carnet del alumno que desea buscar: ");
-        carnet = input.nextInt(); //Lee el carnet ingresado por el usuario y lo almacena en la variable carnet
+        String carnet = input.next(); //Lee el carnet ingresado por el usuario y lo almacena en la variable carnet
         input.nextLine();
 
         // Verificamos si la llave (carnet) existe dentro del mapa
@@ -63,16 +110,16 @@ public class Main {
      * 2. Gestiona el buffer del Scanner para evitar saltos de línea inesperados.
      * 3. Utiliza tipos de retorno de la API de Map para verificar el éxito de la operación.
      */
-    private static void eliminarAlumno(Scanner input, Map<Integer, String> alumnos) {
+    private static void eliminarAlumno(Scanner input, Map<String, String> alumnos) {
 
-        int carnet = 0; // Inicializamos la variable para almacenar el carnet del alumno a eliminar
+        String carnet = input.nextLine(); // Inicializamos la variable para almacenar el carnet del alumno a eliminar
         boolean datoValido = false; // Bandera para controlar el bucle
 
         // Bucle de validación: se repetirá mientras 'datoValido' sea falso
         while (!datoValido) {
             try {
                 System.out.println("Digite el carnet del alumno que desea eliminar: ");
-                carnet = input.nextInt(); // Intenta leer el entero
+                carnet = input.next(); // Intenta leer el entero
                 input.nextLine();         // Limpia el buffer (el "Enter" sobrante)
                 datoValido = true;        // Si llega aquí, el dato es correcto y saldrá del bucle
             } catch (Exception e) {
@@ -112,7 +159,7 @@ public class Main {
     *   Metodo para mostrar/listar todos los alumnos
     *   Aplicando Guard Clause para validar si el HashMap de alumnos esta vacio antes de mostrar informacion
     *  */
-    private static void mostrarAlumnos(Map<Integer, String> alumnos) {
+    private static void mostrarAlumnos(Map<String, String> alumnos) {
         if (alumnos.isEmpty()) {
             System.out.print("\n No hay alumnos ingresados en el sistema");
             return;
@@ -121,5 +168,22 @@ public class Main {
         System.out.print("\n--- Alumnos ingresados en el sistema ---");
 
         alumnos.forEach((id, nombre) -> System.out.print("\n Carnet: " + id + " | Alumno: " + nombre));
+    }
+
+    private static void actualizarAlumno(Scanner input, Map<String, String> alumnos) {
+        System.out.println("\n--- Apartado para actualizar datos de alumno ---");
+        input.nextLine();
+        System.out.print("Digite el carnet del alumno que desea modificar: ");
+        String carnet = input.nextLine();
+        if(alumnos.containsKey(carnet)){
+            System.out.println("Alumno encontrado: " + alumnos.get(carnet));
+            System.out.print("Ingrese el nuevo nombre completo: ");
+            String nuevoNombre = input.nextLine();
+
+            alumnos.put(carnet, nuevoNombre);
+            System.out.println("Datos actualizados con exito ");
+        }else {
+            System.out.println("Alumno no encontrado");
+        }
     }
 }
